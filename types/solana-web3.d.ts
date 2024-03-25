f4d410b2a35bcca1ba12fdb5e4b84ea250867a34
Gianluca Mazza <gianluca.mazza@scalingparrots.com>
2024-03-25 16:31:41 +0100

    cleanup types

--- types/solana-web3.d.ts
+++ types/solana-web3.d.ts
@@ -11,7 +11,6 @@ declare module '@solana/web3.js' {
 
     export class Keypair {
         constructor();
-        static generate(): Keypair;
         publicKey: PublicKey;
         secretKey: Uint8Array;
     }
@@ -21,14 +20,11 @@ declare module '@solana/web3.js' {
     export class Connection {
         constructor(endpoint: string, commitment?: Commitment);
         getBalance(publicKey: PublicKey, commitment?: Commitment): Promise<number>;
-        getRecentBlockhash(commitment?: Commitment): Promise<any>;
 
     }
 
     export class Transaction {
         constructor();
-        add(instruction: any): void;
         sign(...signers: Array<Keypair>): void;
-        serializeMessage(): Buffer;
     }
 }
